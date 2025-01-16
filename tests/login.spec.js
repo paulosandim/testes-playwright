a814c712bdbef628466d2b5a4603f9b22f1987b8
Paulo Sandim <pauloedusandim@gmail.com>
2025-01-15 23:23:39 -0300

    first test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,1 +1,6 @@
-import test from "playwright"
+import { expect, test} from "@playwright/test"
+
+test('the user login with sucess', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/')
+  await expect(await page.title()).toBe('Swag Labs')
+})
